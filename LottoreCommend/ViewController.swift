4462f6f1ca51b6e0f0064ba00407a56b64b42a3d
k4keye <vps32@naver.com>
2021-12-22 22:00:24 +0900

    Initial Commit

---
+++ LottoreCommend/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LottoreCommend
+//
+//  Created by k4keye on 2021/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
