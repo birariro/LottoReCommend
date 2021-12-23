71e04a893bf7061f7196e91742bdbe15c25383b8
k4keye <vps32@naver.com>
2021-12-23 19:33:48 +0900

    intro 스토리보드 제약조건 추가

--- LottoreCommend/IntroController.swift
+++ LottoreCommend/IntroController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class IntroController: UIViewController {
    
     @IBOutlet weak var progressView: UIProgressView!
     //@IBOutlet weak var progressView: UIProgressView!
